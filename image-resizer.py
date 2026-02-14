9e7b285a5f32412731a6e8257f8b0be1ad3d722b
Adarsh-kumar91 <jaminmathia7631@gmail.com>
2026-02-14 20:11:53 +0530

    Initial commit - Image-resizer system

---
+++ image-resizer.py
@@ -0,0 +1,20 @@
+import cv2
+
+source = "images.jpeg"
+destination = "newImage.png"
+scale_percent = 50
+
+
+src = cv2.imread(source , cv2.IMREAD_UNCHANGED)
+# cv2.imshow("Title", src)
+
+
+new_width = int(src.shape[1] * scale_percent / 100)
+new_height = int(src.shape[0] * scale_percent / 100)
+
+dsize = (new_width, new_height)
+
+output = cv2.resize(src, dsize)
+
+cv2.imwrite(destination , output)
+# cv2.waitKey(0)
